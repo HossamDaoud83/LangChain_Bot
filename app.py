543e21289b573ea0cb1c1497033b6d1d7fbb156c
Hossam Daoud <101238592+HossamDaoud83@users.noreply.github.com>
2023-09-23 19:57:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import streamlit as st
 import os
-import chainlit as cl
-from langchain import HuggingFaceHub, PromptTemplate, LLMChain
+from faq_gsb import faq_in, faq_out
+from langchain import HuggingFaceHub, PromptTemplate
 import spacy
-import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
+
 
 # Set the Hugging Face Hub API token as an environment variable
 os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
@@ -20,77 +21,75 @@ template = """
 """
 
 # Define your FAQ entries and responses
-faq_entries = [
-    "Who are you?",
-    "Who is the college Name?",
-    "Who is the college Dean?",
-    "Who is your Developer?",
-    "How can I contact you?",
-    "Could I installments the Tuition Fees?",
-    "What is the Program Study Plan?",
-    "How can i register in your college?",
-    # Add more FAQ entries for different topics
-]
+faq_entries = faq_in
 
-faq_responses = [
-    "I'm Laila, the GSB's AI Virtual Assistant.",
-    "Graduate School of Business at The Arab Academy for Science, Technology and Maritime Transport (AASTMT)",
-    "Prof.Aiman Ragab, current college dean since 2016, He has a PhD in Accounting and finance from USA.",
-    "Eng. Hossam Daoud, GSB E-learning Administrator.",
-    """College Contacts is Only: 01111781111 or 19838
-    Facebook: https://www.fb.com/MBA.AAGSB.AAST.EDU
-    Website: https://aast.edu/en/colleges/gsb/alex
-    Address: Arab Academy, Gamal Abdel Nasser, Miami, Alexandria, Egypt
-    E-mail: student@aagsb.aast.edu
-    GSB Moodle: https://gsb.aast.edu""",
-    "Tuition fees cannot be paid in installments",
-    "Study plans through the college website only",
-    "complete the online form to enroll:https://forms.gle/5dPnX6BLHSLLwLSg7, or you can contact us on 01111781111 or 19838",
-    # Add corresponding responses for each FAQ entry
-]
+faq_responses = faq_out
+
 
 # Create a dictionary to store FAQ entries and responses
 faq_dict = dict(zip(faq_entries, faq_responses))
 
+
 # Load spaCy with word vectors (make sure to install and download the appropriate model)
+# python -m spacy download en_core_web_sm
 nlp = spacy.load("en_core_web_sm")
 
-@cl.on_chat_start
+
+# Define a function to generate a response using HuggingFaceHub
+def generate_response(user_input):
+    template = "{query}"  # Simplify the template
+
+    # Generate a response using HuggingFaceHub
+    response = conv_model.predict(template.format(query=user_input))
+
+    return response
+
+
+# Create a Streamlit web app
 def main():
-    prompt = PromptTemplate(template=template, input_variables=['query'])
-    conv_chain = LLMChain(llm=conv_model, prompt=prompt, verbose=True)
-    cl.user_session.set("llm_chain", conv_chain)
+    st.set_page_config(page_title="GSB", page_icon="Pic.png")
+    st.image("Picture1.png", width=150,)
+    st.title("Laila, the GSB's AI Virtual Assistant")
+    hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
 
-@cl.on_message
-async def main(message: str):
-    llm_chain = cl.user_session.get("llm_chain")
-    user_input = message.lower()  # Convert user input to lowercase for easier matching
+    # Get user input
+    user_input = st.text_input("Ask a question:")
 
-    # Calculate the cosine similarity between user input and FAQ entries
-    similarities = []
-    for entry in faq_entries:
-        entry_doc = nlp(entry.lower())
-        user_doc = nlp(user_input)
-        similarity = cosine_similarity(
-            [entry_doc.vector],
-            [user_doc.vector]
-        )[0][0]
-        similarities.append(similarity)
+    if st.button("Submit"):
+        if user_input:
+            user_input = user_input.lower()  # Convert user input to lowercase for easier matching
+            # Calculate the cosine similarity between user input and FAQ entries
+            similarities = []
+            for entry in faq_entries:
+                entry_doc = nlp(entry.lower())
+                user_doc = nlp(user_input)
+                similarity = cosine_similarity(
+                    [entry_doc.vector],
+                    [user_doc.vector]
+                )[0][0]
+                similarities.append(similarity)
 
-    # Find the FAQ entry with the highest similarity
-    max_similarity = max(similarities)
-    max_index = similarities.index(max_similarity)
+            # Find the FAQ entry with the highest similarity
+            max_similarity = max(similarities)
+            max_index = similarities.index(max_similarity)
 
-    # Define a threshold for considering a match
-    threshold = 0.2  # Adjust as needed
+            # Define a threshold for considering a match
+            threshold = 0.5  # Adjust as needed
 
-    if max_similarity >= threshold:
-        # If the similarity is above the threshold, provide the corresponding response
-        await cl.Message(content=faq_responses[max_index]).send()
-    else:
-        # If no match is found, generate a response using HuggingFaceHub
-        response = await llm_chain.acall(template.format(query=message), callbacks=[cl.AsyncLangchainCallbackHandler()])
-        await cl.Message(content=response["text"]).send()
+            if max_similarity >= threshold:
+                # If the similarity is above the threshold, provide the corresponding FAQ response
+                st.text_area("Laila: ", value=faq_responses[max_index], height=300)
+            else:
+                # If no match is found, generate a response using HuggingFaceHub
+                response = generate_response(user_input)
+                st.text_area("Laila_AI: ", value=response, height=300)
+
 
 if __name__ == "__main__":
-    cl.run()
+    main()
