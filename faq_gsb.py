8d9cb549127a56afb4be7cb45114ea549442d479
Hossam Daoud <101238592+HossamDaoud83@users.noreply.github.com>
2023-09-23 19:58:43 +0300

    Update faq_gsb.py

--- faq_gsb.py
+++ faq_gsb.py
@@ -1,1 +1,118 @@
+# Define your FAQ entries and responses
+faq_in = [
+    "hey, How are you?",
+    "Who are you?",
+    "Who is the college Name?",
+    "Who is the college Dean?",
+    "Who is your Developer?",
+    "How can I contact you?",
+    "Could I installments the Tuition Fees?",
+    "What is the Program Study Plan?",
+    "How can i register in your college?",
+    "ask about MBA Master of Business Administration",
+    "ask about DBA Doctorate of Business Administration",
+    "what is Marywood MBA Dual Degree",
+    "what is Marywood PHD Dual Degree",
+    " what is the college location address?",
+    # Add more FAQ entries for different topics
+]
+
+faq_out = [
+    "I'm fine, thank you.",
+    "I'm Laila, the GSB's AI Virtual Assistant.",
+    "Graduate School of Business at The Arab Academy for Science, Technology and Maritime Transport (AASTMT)",
+    "Prof.Aiman Ragab, current college dean since 2016, He has a PhD in Accounting and finance from USA.",
+    "Eng. Hossam Daoud, GSB E-learning Administrator.",
+    """College Contacts is Only: 01111781111 or 19838
+    Facebook: https://www.fb.com/MBA.AAGSB.AAST.EDU
+    Website: https://aast.edu/en/colleges/gsb/alex
+    Address: Arab Academy, Gamal Abdel Nasser, Miami, Alexandria, Egypt
+    E-mail: student@aagsb.aast.edu
+    GSB Moodle: https://gsb.aast.edu""",
+    "Tuition fees cannot be paid in installments",
+    """Study plans through the college website only: https://aast.edu/en/colleges/gsb/Alex/ For more information, 
+    please specify your program of study by writing it in the message calling 01111781111 or 19838.""",
+    "contact us on 01111781111 or 19838 or complete the online form to enroll: https://forms.gle/5dPnX6BLHSLLwLSg7",
+    """Master in Business Administration (MBA) This accredited MBA applies to all graduates from different 
+    disciplines provided that they are in compliance with the admission requirements set by GSB for all applicants to 
+    the MBA degree.
+
+Admission Requirements:
+1.	Original bachelor degree & original transcript.
+2.	Accreditation from the High Supreme Council of Universities for non-Egyptians and private universities.
+3.	For non-Egyptian Original Embassy approval letter.
+4.	4 photos & ID copy / passport copy.
+5.	Advanced Management Admission Test (AMAT v1) Exam on GSB Moodle..
+6.     TOEFL Score Inst. (500), International (79) or IELTS Score (6) or Online English test by Cambridge.
+
+Admission Fees: $182
+Tuition Fees (per Term): 
+•  $1750 for Egyptians  or $2500 for non-Egyptians""",
+
+    """Doctorate in Business Administration (DBA) The DBA is equivalent to a conventional Ph.D. and its designation 
+    as a Doctorate of Business Administration signifies its focus on theoretical and applied knowledge of the 
+    advancement of management and business practices. The DBA is designed to create the leadership and efficacy that 
+    are needed to tackle the challenges of senior management on an international level.
+
+Admission Requirements:
+1.	Original bachelor degree & original transcript.
+2.	Original master degree & original transcript.
+3.	Accreditation from the High Supreme Council of Universities for non-Egyptians and private universities.
+4.	For non-Egyptian Original Embassy approval letter.
+5.	4 photos & ID copy / passport copy.
+6.	Advanced Management Admission Test (AMAT v2) Exam on GSB Moodle..
+7.  TOEFL Score Inst. (500), International (79) or IELTS Score (6) or Online English test by Cambridge.
+
+Admission Fees: $232
+Tuition Fees (per Term): -
+•	$3033 for Egyptians or $5673 for non-Egyptians for Term-1, Term-2 and Term-3)
+•	$4023 for Egyptians or $7543 for non-Egyptians for Term-4, Term-5 and Term-6)""",
+
+    """Marywood MBA Dual Degree
+The student receives two MBA degrees: 
+•	An equivalent MBA from the Egyptian Supreme Council of Universities for the Arab Academy for Science and Technology.
+•	In addition to a Marywood University certificate.
+
+With the opportunity to complete the second year of an MBA program in the United States of America.
+A student with a Master of Business Administration (MBA) degree can pursue doctoral studies at Marywood University.
+
+Admission Requirements:
+1.	Original bachelor degree & original transcript.
+2.	Accreditation from the High Supreme Council of Universities for non-Egyptians and private universities.
+3.	For non-Egyptian Original Embassy approval letter.
+4.	4 photos & ID copy / passport copy.
+5.	Advanced Management Admission Test (AMAT) Exam on GSB Moodle..
+6.     TOEFL Score Inst. (500), International (79) or IELTS Score (6) or Online English test by Cambridge.
+
+Admission Fees: $182
+
+Tuition Fees (per Term): 
+•  $2350 for Egyptians  or $3100 for non-Egyptians""",
+
+    """Marywood PHD Dual Degree Students who enroll in the Marywood University PhD dual degree program will receive 
+    two prestigious certificates upon completion:
+
+PhD Degree from Marywood University: Graduates will earn a highly regarded PhD degree from Marywood University, 
+recognized for its excellence in higher education.
+
+DBA Degree from the Arab Academy for Science and Technology (AAST): In addition to the PhD degree, students will also 
+receive a Doctor of Business Administration (DBA) degree from the Arab Academy for Science and Technology, 
+accredited by the Egyptian Supreme Council of Universities.
+
+Admission Requirements:
+1. Original bachelor degree & original transcript.
+2. Original master degree & original transcript.
+3. Accreditation from the High Supreme Council of Universities for non-Egyptians and private universities.
+4. For non-Egyptian Original Embassy approval letter.
+5. 4 photos & ID copy / passport copy.
+6. Advanced Management Admission Test (AMAT v2) Exam on GSB Moodle..
+7. TOEFL Score Inst. (500), International (79) or IELTS Score (6) or Online English test by Cambridge.
+
+Admission Fees: $232
+
+Tuition Fees (per Term): 
+•  $4033 for Egyptians  or $6673 for non-Egyptians""",
+    """The Arab Academy for Science, Technology and Maritime Transport (AASTMT) Alexandria Main Branch""",
+    # Add corresponding responses for each FAQ entry
+]
 
